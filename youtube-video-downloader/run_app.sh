e88c6984c1261f5b2bfdd3dbccb06f64126e4258
windzer99311 <windzergamer@gmail.com>
2025-10-01 22:23:37 +0530

    Delete youtube-video-downloader directory

--- youtube-video-downloader/run_app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Streamlit uygulamasını çalıştırma scripti
-
-# Çalıştırmak istediğin Python ortamını aktif et (opsiyonel)
-# source venv/bin/activate
-
-# Streamlit uygulamasını başlat
-streamlit run Home.py
